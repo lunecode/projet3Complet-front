e9c4a2ad79589529d38bf1bf8e6e8bc5056e1fd1
AsmaMoutakil <nassirasma5@gmail.com>
2019-06-09 21:47:54 +0200

    route for ingredient ,not totally finished

--- src/components/ProfilComponents/Postingredients.js
+++ src/components/ProfilComponents/Postingredients.js
@@ -12,28 +12,46 @@ class Postingrédients extends Component {
     e = e.target || e.srcElement;
     console.log(e.id);
     
-    axios.post('http://localhost:3000/profil/insert_profil', this.state)
-    .then(response => {
-      console.log(response)
-    })
-    .catch(error => {
-      console.log(error)
-    })
+    // axios.post('http://localhost:3000/profil/insert_profil', this.state)
+    // .then(response => {
+    //   console.log(response)
+    // })
+    // .catch(error => {
+    //   console.log(error)
+    // })
 }
   
   render() {
     const ingredients = this.state
     return (
       <div onClick={this.handleClick}>
-        <img src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
+        <img src="https://i.ibb.co/NSqJfc5/Capture-d-e-cran-2019-06-09-a-21-31-34.png"
           name="ingredients"
-          id="aventure seule"
+          id="passioné de voyage"
           value={ingredients}
           alt="L"
         />
-        <img src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
+        <img src="https://i.ibb.co/BC1SvVL/Capture-d-e-cran-2019-06-09-a-21-31-44.png"
           name="ingredients"
-          id="les copains d'abord"
+          id="Créateur de contenu"
+          value={ingredients}
+          alt="L"
+        />
+        <img src="https://i.ibb.co/0m2HBgp/Capture-d-e-cran-2019-06-09-a-21-32-15.png"
+          name="ingredients"
+          id="Photographe & Vidéaste"
+          value={ingredients}
+          alt="L"
+        />
+          <img src="https://i.ibb.co/XyKHRzx/Capture-d-e-cran-2019-06-09-a-21-32-29.png"
+          name="ingredients"
+          id="Bloggeur voyage"
+          value={ingredients}
+          alt="L"
+        />
+          <img src="https://i.ibb.co/brkcGx5/Capture-d-e-cran-2019-06-09-a-21-32-50.png"
+          name="ingredients"
+          id="Autre"
           value={ingredients}
           alt="L"
         />
