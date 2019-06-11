d6af4218516bafff5d0c43f4fbe764ca176663e9
Yous <ysouane0@gmail.com>
2019-06-11 12:03:23 +0200

    create json continent

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-// import UploadVideo from './screen/UploadPage/UploadVideo';
+import UploadVideo from './screen/UploadPage/UploadVideo';
 // import UploadInformation from './screen/UploadPage/UploadTravelInformation';
-import UploadTravelStep from './screen/UploadPage/UploadTravelStep';
+// import UploadTravelStep from './screen/UploadPage/UploadTravelStep';
 
 
 class App extends Component {
   render() {
     return (
       <>
-        {/* <UploadVideo /> */}
+        <UploadVideo />
         {/* <UploadInformation /> */}
-        <UploadTravelStep />
+        {/* <UploadTravelStep /> */}
       </>
     )
   }
--- src/screen/UploadPage/UploadVideo.js
+++ src/screen/UploadPage/UploadVideo.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-import PostVideo from '../../components/UploadComponents/PostVideo';
-import GetVideo from '../../components/UploadComponents/GetVideo';
-import PutVideo from '../../components/UploadComponents/PutVideo';
-import DeleteVideo from '../../components/UploadComponents/DeleteVideo';
+import PostVideo from '../../components/uploadComponents/PostVideo';
+import GetVideo from '../../components/uploadComponents/GetVideo';
+import PutVideo from '../../components/uploadComponents/PutVideo';
+import DeleteVideo from '../../components/uploadComponents/DeleteVideo';
 
 
 class UploadVideo extends Component {
   render() {
     return (
       <>
+        <select>
+          <option>Mercure</option>
+          <option>Vénus</option>
+          <option selected="selected">Terre</option>
+          <option>Mars</option>
+          <option>Jupiter</option>
+          <option>Saturne</option>
+          <option>Uranus</option>
+          <option>Neptune</option>
+          <option>Pluton</option>
+        </select>
         <h1>Affichage de la table general_video :</h1>
         <GetVideo />
         <h1>Insertion de données dans la table general_video</h1>
         <PostVideo />
         <h1>Modification de données dans la table general_video</h1>
         <PutVideo />
-       {/* Problem with the foreign key  */}
         <h1>Suppression des données de la table general_video</h1>
         <DeleteVideo />
       </>
