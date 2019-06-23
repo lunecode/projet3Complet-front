883b6662e650a2aeaf191494c1ca7ba57411e090
AsmaMoutakil <nassirasma5@gmail.com>
2019-06-23 21:36:01 +0200

    test get video

--- src/components/uploadComponents/PostVideo.js
+++ src/components/uploadComponents/PostVideo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios"
+import GetVideo from './GetVideo';
 
 class PostVideo extends Component {
     state={
@@ -7,28 +8,39 @@ class PostVideo extends Component {
 
     }
 
-    fileSelectedHandler= e=>{
-        this.setState({ [e.target.name]: e.target.value})
-    }
-    fileUploadHandler =(e)=>{
-        e.preventDefault()
-        console.log(this.state)
-        axios.post('http://localhost:3000/general_video/insert_general_video', this.state)
-          .then(response => {
-            console.log(response)
-          })
-          .catch(error => {
-            console.log(error)
-          })
+    // fileSelectedHandler= e=>{
+    //     this.setState({ [e.target.name]: e.target.value})
+    // }
+    // fileUploadHandler =(e)=>{
+    //     e.preventDefault()
+    //     console.log(this.state)
+    //     axios.post('http://localhost:3000/general_video/insert_general_video', this.state)
+    //       .then(response => {
+    //         console.log(response)
+    //       })
+    //       .catch(error => {
+    //         console.log(error)
+    //       })
       
-    }
+    // }
+handelchange=(e)=>{
+let files=e.target.files;
+let reader=new FileReader();
+reader.readAsDataURL(files[0]);
+reader.onload=(e)=>{
+this.setState({
+  video_link:e.target.result
+})
+//  console.log("img data",e.target.result)
+ }
+}
  
 
     render() {
   console.log(this.state);
         return (
-            <div>
-                <input type='file' name="video_link"
+            <div onSubmit={this.onFormsubmit}> 
+                {/* <input type='file' name="video_link"
                  value={this.state.video_link} 
                 onChange={this.fileSelectedHandler}
                 />
@@ -37,18 +49,26 @@ class PostVideo extends Component {
                 <source src={this.state.video_link}
 
                 type="video"/> Sorry, your browser doesn't support embedded videos.
-                </video>
+                </video> */}
+                     <input type='file' name="video_link"
+             value={this.state.video_link} 
+                onChange={this.handelchange}
+                />
+                <button onClick={this.fileUploadHandler}>Télécharger</button>
+                <video controls width="700">
+                <source 
 
+                type="video"/> Sorry, your browser doesn't support embedded videos.
+                </video>
+                
+{/* <GetVideo /> */}
             </div>
 
 
-          
-           
-       
+        )
 
-        );
-    }
-}
+       }   
+ }
 
 
 export default PostVideo;
