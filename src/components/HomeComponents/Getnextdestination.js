3a52204df5ddee115dce12fd00edb9fc5a146acf
DeandreAR <andry.ramarolahy@gmail.com>
2019-06-25 00:05:56 +0200

    Home intregration contributor

--- src/components/HomeComponents/Getnextdestination.js
+++ src/components/HomeComponents/Getnextdestination.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-import './Getnextdestination.css';
+import './Getnextdestination.scss';
 
 import arrow from '../../pictures/arrow.png'
 import leftHome from '../../Images-tripitto/Icon/leftHome.png'
@@ -53,14 +53,11 @@ class Getnextdestination extends Component {
             <>
                 {/****************************** SECTION NEXT DESTINATION******************************* */}
                 <section className='nextdestination'>
+                    
                     <div class="wrapper-nextpicture">
                         <div className='container_arrow'>
-                            
                                 <img onClick={this.selectbestplan} className="leftarrow" src={arrow} alt=""></img>
-                            
-                          
                                 <img onClick={this.selectbestplanBack} className="rightarrow" src={arrow} alt=""></img>
-                            
                         </div>
                         {this.state.nextpicture.map(nextpicture => (
                             <div className={"divpictureNext" + i} key={i++}>
@@ -73,23 +70,6 @@ class Getnextdestination extends Component {
                     </div>
                 </section>
 
-                {/****************************** CONTRIBUTORS  ***************************** */}
-                    {/* <section className="Contributors">
-                    <div className='container_contributors_title'>
-                        <h2 className="title_contributors">Merci à nos contributeurs</h2>
-                    </div>
-                    <ul>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                    </ul>
-                </section> */}
-            
-            
                 {/****************************** SECTION BEST PLAN  ***************************** */}
                 <section className="bestplan">
                     <div className='container_bestplan_title'>
@@ -118,18 +98,30 @@ class Getnextdestination extends Component {
                             <p className='title_bestplan_nbviews'>{video.nb_views} vues</p>
                         </div>
                     ))}
-                    {/* <div className='container_contributors_title'>
+                    <div className='container_contributors_title'>
                         <h2 className="title_contributors">Merci à nos contributeurs</h2>
+                    <div className='contributor_name'>
+                        <p>Adam Denisov</p>
+                        <p>Alexander Ljung</p>
+                        <p>Aneta Skodova</p>
+                        <p>Athar Malakooti</p>
+                        <p>Boniface Esanji</p>
+                        <p>Cadarion Hart</p>
+                        <p>Charles Davies</p>
+                        <p>Chioke Chinasa</p>
+                        <p>Clarke Gillebert</p>
+                        <p>Dusya Sigachoya</p>
+                        <p>Faadi Arache</p>
+                        <p>Fyodor Dyzenhkov</p>
+                        <p>Heather Clarke</p>
+                        <p>Homoru Yunosuke</p>
+                        <p>Ivan Polo</p>
+                        <p>Jafaros Long</p>
+                        <p>Jesus Banuelos</p>
+                        <p>Kiandra Lowe</p>
+                        <p>Labeeba Almer</p>
                     </div>
-                    <ul>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                        <li>Marco Polo</li>
-                    </ul> */}
+                    </div>
                 </div>
 
 
