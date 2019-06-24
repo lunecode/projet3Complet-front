baddc86b9129e810f0485c4e3908d7859978dba2
AsmaMoutakil <nassirasma5@gmail.com>
2019-06-24 10:27:13 +0200

    test video

--- src/components/uploadComponents/PostVideo.js
+++ src/components/uploadComponents/PostVideo.js
@@ -8,31 +8,36 @@ class PostVideo extends Component {
 
     }
 
-    // fileSelectedHandler= e=>{
-    //     this.setState({ [e.target.name]: e.target.value})
-    // }
-    // fileUploadHandler =(e)=>{
-    //     e.preventDefault()
-    //     console.log(this.state)
-    //     axios.post('http://localhost:3000/general_video/insert_general_video', this.state)
-    //       .then(response => {
-    //         console.log(response)
-    //       })
-    //       .catch(error => {
-    //         console.log(error)
-    //       })
+    fileSelectedHandler= e=>{
+        this.setState({ [e.target.name]: e.target.value})
+    }
+    fileUploadHandler =(e)=>{
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('http://localhost:3000/general_video/insert_general_video', this.state)
+          .then(response => {
+            console.log(response)
+          })
+          .catch(error => {
+            console.log(error)
+          })
       
-    // }
+    }
 handelchange=(e)=>{
 let files=e.target.files;
 let reader=new FileReader();
 reader.readAsDataURL(files[0]);
 reader.onload=(e)=>{
-this.setState({
-  video_link:e.target.result
-})
-//  console.log("img data",e.target.result)
+  this.setState({video_link : e.target.result}, ()=> {
+    console.log("video data",this.state.video_link)
+  })
  }
+}
+
+blob2file=(blobData)=> {
+  const fd = new FormData();
+  fd.set('a', blobData);
+  return fd.get('a');
 }
  
 
@@ -40,27 +45,13 @@ this.setState({
   console.log(this.state);
         return (
             <div onSubmit={this.onFormsubmit}> 
-                {/* <input type='file' name="video_link"
-                 value={this.state.video_link} 
-                onChange={this.fileSelectedHandler}
-                />
-                <button onClick={this.fileUploadHandler}>Télécharger</button>
-                <video controls width="700">
-                <source src={this.state.video_link}
-
-                type="video"/> Sorry, your browser doesn't support embedded videos.
-                </video> */}
-                     <input type='file' name="video_link"
-             value={this.state.video_link} 
+            <input type='file' name="video_link"
+            //  value={this.state.video_link} 
                 onChange={this.handelchange}
                 />
+                 <img src={this.blob2file} alt=""/>
                 <button onClick={this.fileUploadHandler}>Télécharger</button>
-                <video controls width="700">
-                <source 
-
-                type="video"/> Sorry, your browser doesn't support embedded videos.
-                </video>
-                
+            
 {/* <GetVideo /> */}
             </div>
 
